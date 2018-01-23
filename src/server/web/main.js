c89502cd2865826699d16737ffc00e39f9c88dc4
Akinori Tomita <akinori.tomita@gmail.com>
2018-01-23 14:02:07 +0900

    本番サーバー

---
+++ src/server/web/main.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const config = require('../../../config');
+
+function run() {
+  const host = process.env.HOST || 'localhost';
+  const port = process.env.PORT || 8080;
+
+  const app = express();
+  app.disable('x-powered-by');
+  app.use(express.static(config.build.assetsRoot));
+  app.listen(port, host, (err) => {
+    if (!err) {
+      global.console.info(`server started ${host}:${port}`);
+    } else {
+      global.console.error(err);
+    }
+  });
+}
+
+module.exports = run;
